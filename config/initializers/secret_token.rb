1307e57e56689d60585fe1c91605301a7616bb9a
Ed Halferty <edhalferty@gmail.com>
2013-07-23 00:17:08 -0700

    Fixed the loading of secret keys, updated readme

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Csr2::Application.config.secret_token = '37fe0797d312977ab181a9575430db55772c050c88d0249b77ed6828fa924a4dbfaf14137368f51f03bed1a03ff7a13137512c65927609059f4e13ba7d0b0d5b'
+Csr2::Application.config.secret_token = ENV["RAILS_SECRET_TOKEN"]
